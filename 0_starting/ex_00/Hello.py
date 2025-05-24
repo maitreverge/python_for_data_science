8e00ba0bec3de6ca88643e7d3e6a21f6baeff6a0
maitreverge_mbp <verge.f@gmail.com>
2025-05-24 12:09:33 +0200

    ex_00 and ex_01

---
+++ 0_starting/ex_00/Hello.py
@@ -0,0 +1,14 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "France!")
+ft_set = {"Hello", "Perpignan!"}
+ft_dict = {"Hello" : "42Perpignan!"}
+
+print(ft_list)
+print(ft_tuple)
+
+"""
+Sets in Python are unordered collections of unique elements.
+The order in which elements appear when you print a set is not guaranteed and may vary.
+"""
+print(ft_set)
+print(ft_dict)
---
+++ 0_starting/ex_01/format_ft_time.py
@@ -0,0 +1,18 @@
+import time
+import datetime
+
+# See https://www.w3schools.com/python/gloss_python_date_format_codes.asp
+
+def main():
+    time_in_seconds = time.time()
+
+    # :, Displays the thousands separators, .4f is the decimal precision.
+    print(f"Seconds since January 1, 1970: {time_in_seconds:,.4f} ", end="")
+    print(f"or {time_in_seconds:e} in scientific notation")
+
+    # Extract the current date, and format it.
+    simple_date = datetime.datetime.now().strftime("%a %d ") 
+    print(datetime.datetime.now().strftime("%b %d %Y"))
+
+if __name__ == "__main__":
+    main()
