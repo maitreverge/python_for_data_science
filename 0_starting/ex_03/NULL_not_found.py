c5d1c8a6e12b28c45f3cf26f31907a68eff7d1ef
“maitreverge_42” <verge.f@gmail.com>
2025-05-26 11:46:13 +0200

    done ex03

--- 0_starting/ex_03/NULL_not_found.py
+++ 0_starting/ex_03/NULL_not_found.py
@@ -1,2 +1,28 @@
 def NULL_not_found(object: any) -> int:
-    print(object)
+    current_type = type(object)
+
+    # Extract the type name to make a match/case easier.
+    type_name = str(current_type).split("'")[1]
+
+    name = ""
+    match type_name:
+        case "NoneType":
+            name = "Nothing"
+        case "float":
+            name = "Cheese"
+        case "int":
+            name = "Zero"
+        case "str":
+            if object:
+                print("Type not Found")
+                return 1
+            else:
+                name = "Empty"
+        case "bool":
+            name = "Fake"
+        case "None":
+            name = "Fake"
+    
+    print(f"{name}: {object} {current_type}")
+    
+    return 0
