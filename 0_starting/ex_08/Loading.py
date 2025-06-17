8ffc40a0412d671c30eae0aa760b485581a47cb2
maitreverge_mbp <verge.f@gmail.com>
2025-06-17 12:22:47 +0200

    testting ft_tqdm

--- 0_starting/ex_08/Loading.py
+++ 0_starting/ex_08/Loading.py
@@ -1,3 +1,9 @@
-def ft_tqdm(lst: range) -> None:
+def ft_tqdm(lst: range):
+
+    percentage = 0
+    len_lst = len(lst)
+
     for i in lst:
         yield i
+        percentage += 1
+        print(f"{percentage}% | |",end="", flush=True)
