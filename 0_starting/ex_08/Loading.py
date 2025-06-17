911d784a57635443b687910107afc4d16d5bc1e5
maitreverge_mbp <verge.f@gmail.com>
2025-06-17 13:13:46 +0200

    90% good ft_tqdm

--- 0_starting/ex_08/Loading.py
+++ 0_starting/ex_08/Loading.py
@@ -2,7 +2,14 @@ FULL_BLOCK = '█'
 # Since we can't use any module to get the terminal size, I hardcoded the total progress_bar_lenght
 PROGRESS_BAR_LENGHT = 80
 
+def get_terminal_size():
+    ...
+
 def ft_tqdm(lst: range):
+    """
+    ! DOTO :
+    Match the 10 steps of the original function, and implement an optional get_terminal_size function
+    """
 
     percentage = 0
     len_lst = len(lst)
@@ -11,10 +18,10 @@ def ft_tqdm(lst: range):
     for i in lst:
         percentage += (1 / len_lst) * 100
 
-        full_blocks = range(int(percentage / 80))
+        full_blocks = percentage * PROGRESS_BAR_LENGHT / 100 / (PROGRESS_BAR_LENGHT / 80)
 
-        for _ in full_blocks:
-            progress_bar = progress_bar.replace(' ', FULL_BLOCK, 1)
+        # for _ in full_blocks:
+        progress_bar = (FULL_BLOCK * int(full_blocks)) + (' ' * (PROGRESS_BAR_LENGHT - int(full_blocks)))
 
         print(f"\r{percentage:.0f}%|{progress_bar}| {i + 1}/{len_lst}",end="", flush=True)
         yield i
