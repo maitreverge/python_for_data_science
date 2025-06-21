16048dab1a0f383ea7431fc1a4ee641acf9e971d
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-21 12:25:10 +0200

    ex_00 array done

--- 1_array/ex_00/give_bmi.py
+++ 1_array/ex_00/give_bmi.py
@@ -1,34 +1,60 @@
 import numpy as np
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+
+def give_bmi(h: list[int | float], w: list[int | float]) -> list[int | float]:
     """
+    Calculates the BMI based on two lists of similar values.
+    Both lists needs to be of same size, with strictly positives values.
     """
-    # Checks if both lists are made of numeric values
-    lists_are_numeric = all(isinstance(x, (int, float)) for x in height) and all(isinstance(x, (int, float)) for x in weight)
-    
-    if len(height) != len(weight):
+    if len(h) != len(w):
         raise ValueError("Provided lists are not of the same len")
-    elif not lists_are_numeric:
-        raise ValueError("Provided list(s) are not made of numeric values")
-    
-    height_np = np.array(height)
-    weight_np = np.array(weight)
 
-    BMI = weight_np / height_np ** 2
+    try:
+        height_np = np.array(h, dtype=float)
+        weight_np = np.array(w, dtype=float)
+    except Exception:
+        raise ValueError("Provided list(s) must contain only numeric values")
 
-    return BMI.tolist()
+    # `np.all` is the equivalent of the built-in function `all`
+    if not np.all(height_np > 0) or not np.all(weight_np > 0):
+        raise ValueError("Provided list(s) must contain only positive values")
 
+    bmi = weight_np / height_np**2
+    return bmi.tolist()
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """
+    Returns a list where each element of the `bmi` list is compared
+    to the limit, and return a `True` or `False` element.
     """
-    ...
+    try:
+        assert isinstance(limit, int)
+        assert limit > 0
+        bmi_np = np.array(bmi, dtype=float)
+    except (AssertionError, TypeError):
+        raise ValueError("Limit argument is not numeric.")
+    except ValueError:
+        raise ValueError("Provided list must contain only numeric values")
+
+    if not np.all(bmi_np > 0):
+        raise ValueError("BMI can't be nul neither negative")
+
+    result = bmi_np > limit
+
+    return result.tolist()
+
 
 def main():
     """
     Main function
     """
+    height = [1.805, 1.772, 1.668, 1.55, 1.78]
+    weight = [89.1, 130.5, 77.8, 87.0, 56.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+
 
 if __name__ == "__main__":
     main()
--- 1_array/ex_00/tester.py
+++ 1_array/ex_00/tester.py
@@ -1,2 +1,129 @@
 import pytest
+from give_bmi import give_bmi, apply_limit
 
+valid_height_int = [1, 2, 1, 1, 2]
+valid_weight_int = [89, 130, 77, 87, 56]
+
+height_float_same_size = [1.805, 1.772, 1.668, 1.55, 1.78]
+weight_float_same_size = [89.1, 130.5, 77.8, 87.0, 56.4]
+
+height_diff_size = [1.77, 1.80, 1.77, 1.66, 1.55, 1.78]
+weight_diff_size = [89, 130, 77]
+
+height_neg_values = [22, -177, 166, -155, 88]
+weight_neg_values = [-89, 890, -77, 777, -56]
+
+height_zero_values = [180, 177, 0, 155, 178]
+weight_zero_values = [89, 130, 77, 87, 0]
+
+height_incorrect_values = [False, -177, 166, -155, 0]
+weight_incorrect_values = ["a", 130, [77], 87, 56]
+
+limit_bmi_valid = [
+    27,
+    41.56072132851632,
+    27.963240918056922,
+    36.21227887617065,
+    17.80078272945335,
+]
+
+limit_bmi_zero = [
+    0,
+    41.56072132851632,
+    27.963240918056922,
+    36.21227887617065,
+    17.80078272945335,
+]
+
+limit_bmi_neg = [
+    -27,
+    41.56072132851632,
+    27.963240918056922,
+    36.21227887617065,
+    17.80078272945335,
+]
+
+limit_bmi_invalid = [
+    "a",
+    41.56072132851632,
+    27.963240918056922,
+    36.21227887617065,
+    17.80078272945335,
+]
+
+
+def test_happy_path():
+    assert give_bmi(valid_height_int, valid_weight_int) == [
+        89.0,
+        32.5,
+        77.0,
+        87.0,
+        14.0,
+    ]
+    assert give_bmi(height_float_same_size, weight_float_same_size) == [
+        27.347856446773733,
+        41.56072132851632,
+        27.963240918056922,
+        36.21227887617065,
+        17.80078272945335,
+    ]
+
+
+# TESTING GIVE_BMI FUNCTION
+def test_different_size():
+    with pytest.raises(ValueError):
+        give_bmi(valid_height_int, weight_diff_size)
+    with pytest.raises(ValueError):
+        give_bmi(height_diff_size, valid_weight_int)
+
+
+def test_zero_values():
+    with pytest.raises(ValueError):
+        give_bmi(valid_height_int, weight_zero_values)
+    with pytest.raises(ValueError):
+        give_bmi(height_zero_values, valid_weight_int)
+
+
+def test_neg_values():
+    with pytest.raises(ValueError):
+        give_bmi(valid_height_int, weight_neg_values)
+    with pytest.raises(ValueError):
+        give_bmi(height_neg_values, valid_weight_int)
+
+
+def test_neg_and_zero_values():
+    with pytest.raises(ValueError):
+        give_bmi(height_zero_values, weight_neg_values)
+    with pytest.raises(ValueError):
+        give_bmi(height_neg_values, weight_zero_values)
+
+
+def test_incorrect_values():
+    with pytest.raises(ValueError):
+        give_bmi(valid_height_int, weight_incorrect_values)
+    with pytest.raises(ValueError):
+        give_bmi(height_incorrect_values, valid_weight_int)
+
+
+# TESTING APPLY_LIMIT FUNCTION
+def test_limit_happy_path():
+    assert apply_limit(limit_bmi_valid, 27) == [False, True, True, True, False]
+
+
+def test_limit_zero():
+    with pytest.raises(ValueError):
+        apply_limit(limit_bmi_zero, 27)
+
+
+def test_limit_neg_values():
+    with pytest.raises(ValueError):
+        apply_limit(limit_bmi_neg, 27)
+    with pytest.raises(ValueError):
+        apply_limit(limit_bmi_valid, -27)
+
+
+def test_limit_invalid_values():
+    with pytest.raises(ValueError):
+        apply_limit(limit_bmi_invalid, 27)
+    with pytest.raises(ValueError):
+        apply_limit(limit_bmi_valid, "a")
