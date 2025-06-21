dd4448e1a90e9c86a3f43ca5595b3f9708ce8a7f
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-21 10:49:54 +0200

    wip on BMI

--- 1_array/ex_00/give_bmi.py
+++ 1_array/ex_00/give_bmi.py
@@ -3,7 +3,21 @@ import numpy as np
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     """
     """
-    ...
+    # Checks if both lists are made of numeric values
+    lists_are_numeric = all(isinstance(x, (int, float)) for x in height) and all(isinstance(x, (int, float)) for x in weight)
+    
+    if len(height) != len(weight):
+        raise ValueError("Provided lists are not of the same len")
+    elif not lists_are_numeric:
+        raise ValueError("Provided list(s) are not made of numeric values")
+    
+    height_np = np.array(height)
+    weight_np = np.array(weight)
+
+    BMI = weight_np / height_np ** 2
+
+    return BMI.tolist()
+
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
