ad6e645b78c633a4b3895f5c8572ded52c8fd153
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-21 13:58:10 +0200

    Fixed function and wrote tests

--- 1_array/ex_01/array2D.py
+++ 1_array/ex_01/array2D.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def slice_me(family: list, start: int, end: int) -> list:
     """
     Slices a 2D list from `start` to `end`, returning a 2D list.
@@ -11,12 +12,12 @@ def slice_me(family: list, start: int, end: int) -> list:
         raise ValueError("Not every subparts of `familly` is a list")
     elif not isinstance(start, int) or not isinstance(end, int):
         raise TypeError("`start` and/or `end` are not integers")
-    
+
     try:
         family_np = np.array(family)
-    except:
-        raise ValueError("All elements in 'family' must be numbers (int or float)")
-    
+    except Exception:
+        raise ValueError("All elements in 'family' must be numbers")
+
     print(f"My shape is : {family_np.shape}")
 
     new_shape = family_np[start:end]
@@ -24,6 +25,7 @@ def slice_me(family: list, start: int, end: int) -> list:
     print(f"My new shape is : {new_shape.shape}")
 
     return new_shape.tolist()
+
 
 def main():
     """
@@ -34,6 +36,6 @@ def main():
     print(slice_me(family, 0, 2))
     print(slice_me(family, 1, -2))
 
+
 if __name__ == "__main__":
     main()
-
--- 1_array/ex_01/tester.py
+++ 1_array/ex_01/tester.py
@@ -5,20 +5,38 @@ from array2D import slice_me
 
 family_valid = [[1.80, 78.4], [10, 102.7], [2.10, 98.5], [1.88, 75.2]]
 family_wrong_size = [[1.80, 78.4], [102.7], [2.10, 98.5], [1.88, 75.2]]
+family_wrong_type1 = [1.80, 78.4]
+family_wrong_type2 = 78.4
+
 
 def test_slice_me_print():
     expected_output = "My shape is : (4, 2)\nMy new shape is : (2, 2)\n"
     expected_result = [[1.80, 78.4], [10, 102.7]]
 
     # Patch capture the result in a string...
-    with patch('sys.stdout', new=StringIO()) as mock_stdout:
+    with patch("sys.stdout", new=StringIO()) as mock_stdout:
         result = slice_me(family_valid, 0, 2)
         # !... which we can get the result from
         actual_output = mock_stdout.getvalue()
 
-    # Assert the printed output matches the expected value
     assert actual_output == expected_output
     assert result == expected_result
 
+
 def test_wrong_size_list():
-    with pytest.raises()
+    with pytest.raises(ValueError):
+        slice_me(family_wrong_size, 0, 2)
+
+
+def test_incorrect_indexes():
+    with pytest.raises(TypeError):
+        slice_me(family_wrong_size, 0, "a")
+    with pytest.raises(TypeError):
+        slice_me(family_wrong_size, "a", 2)
+
+
+def test_incorrect_familly_type():
+    with pytest.raises(ValueError):  # type1 is a list
+        slice_me(family_wrong_type1, 0, 2)
+    with pytest.raises(TypeError):  # type2 is a int
+        slice_me(family_wrong_type2, 0, 2)
