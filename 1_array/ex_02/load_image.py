ff427b04889f7e8d3978ba7ac6d2d523c5b90e07
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-22 16:54:00 +0200

    ex02 done

---
+++ 1_array/ex_02/load_image.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+from PIL import Image
+
+
+def ft_load(path: str) -> np.array:
+    """
+    Takes a file path of extension either `.jpeg`, `.jpg` or `.png`.
+
+    """
+    format_image = ["jpeg", "jpg", "png"]
+    if not os.access(path, os.R_OK):
+        return f"The given file `{path}` is not accessible."
+
+    # try/except if the file does not have any extension
+    try:
+        extension = path.split(".")[-1]
+
+        if extension not in format_image:
+            return f"The file `{path}` extension is not supported."
+    except Exception:
+        return f"The file {path} does not contains a supported extension."
+
+    with Image.open(path) as im:
+        # Simply load an im object into the array.
+        array_image = np.array(im)
+        print(f"The shape of image is : {array_image.shape}")
+        return array_image
+
+
+def main():
+    """
+    Main function
+    """
+    print(ft_load("landscape.jpg"))
+
+
+if __name__ == "__main__":
+    main()
