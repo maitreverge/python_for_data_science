8c99cf3338cf9b6e05fb3868662a2f0bc696b36b
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-25 19:44:13 +0200

    finished ex04

--- 1_array/ex_04/rotate.py
+++ 1_array/ex_04/rotate.py
@@ -4,22 +4,31 @@ import sys
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def ninty_rotate(np_array):
-    """
-    Takes an NumPy array and does a 90 degres clockwise rotation
-    """
 
+def ninty_rotate(original_array: np.ndarray):
     """
+    Takes an NumPy array and performs a 90 degres anti-clockwise rotation.
+
     [1, 2, 3]
     [4, 5, 6]
     [7, 8, 9]
 
-    becomes 
-    [3,6,9]
-    [2,5,8]
-    [1,4,7]
+    becomes
+    [3, 6, 9]
+    [2, 5, 8]
+    [1, 4, 7]
     """
-    ...
+
+    rows, cols = original_array.shape
+
+    rotated = np.empty(original_array.shape)
+
+    for row in range(rows):
+        for col in range(cols):
+            rotated[rows - 1 - col][row] = original_array[row][col]
+
+    return rotated
+
 
 def ft_rotate(np_array) -> None:
     """
@@ -45,16 +54,16 @@ def ft_rotate(np_array) -> None:
     print(f"The shape of image is: {result_array.shape}")
     print(result_array)
 
-    # ! ROTATE THE CROPPED IMAGE
+    rotated_pixels = ninty_rotate(result_array)
 
-    out = ninty_rotate(result_array)
+    print(f"New shape after Transpose: {rotated_pixels.shape}")
 
-    ######################################
-
+    print(rotated_pixels)
 
     # For displaying the X and Y axis
-    plt.imshow(result_array, cmap="gray")
+    plt.imshow(rotated_pixels, cmap="gray")
     plt.show()
+
 
 def main() -> None:
     """
@@ -69,6 +78,7 @@ def main() -> None:
         sys.exit(1)
 
     ft_rotate(np_result)
+
 
 if __name__ == "__main__":
     main()
