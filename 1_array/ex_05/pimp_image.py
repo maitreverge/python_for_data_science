556a676e8bd7ef80e4ab93b9e72a6d3c2fab3c9b
“maitreverge_asahi_linux” <verge.f@gmail.com>
2025-06-26 20:41:53 +0200

    Needs to find a grey_scale formula

--- 1_array/ex_05/pimp_image.py
+++ 1_array/ex_05/pimp_image.py
@@ -1,17 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def show_image(array):
+    im = Image.fromarray(array)
+    im.show()
 
 def ft_invert(img_array: np.ndarray):
-    ...
+    img_array = 255 - img_array
+    show_image(img_array)
 
 def ft_red(img_array: np.ndarray):
-    ...
+    img_array[:,:,1] = 0 # GREEN
+    img_array[:,:,2] = 0 # BLUE
+    show_image(img_array)
+
 
 def ft_green(img_array: np.ndarray):
-    ...
+    img_array[:,:,0] = 0 # RED
+    img_array[:,:,2] = 0 # BLUE
+    show_image(img_array)
 
 def ft_blue(img_array: np.ndarray):
-    ...
+    img_array[:,:,0] = 0 # RED
+    img_array[:,:,1] = 0 # GREEN
+    show_image(img_array)
+
 
 def ft_grey(img_array: np.ndarray):
-    ...
+    # Grayscale conversion formula: Y = 0.299*R + 0.587*G + 0.114*B
+    img_array[:,:,0] = img_array[:,:,0] / 3
+    img_array[:,:,1] = img_array[:,:,0] / 3
+    img_array[:,:,2] = img_array[:,:,0] / 3
+    show_image(img_array)
+
 
--- 1_array/ex_05/tester.py
+++ 1_array/ex_05/tester.py
@@ -2,7 +2,11 @@ from load_image import ft_load
 import pimp_image
 
 def main()->None:
-    ...
+    image_name = "landscape.jpg"
+
+    array_image = ft_load(image_name)
+
+    pimp_image.ft_grey(array_image)
 
 
 
